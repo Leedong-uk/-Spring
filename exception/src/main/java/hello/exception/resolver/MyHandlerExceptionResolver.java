d3fddce394eaf1fba1d1b32417d880c7d1eb218c
이동욱 <du123kim@naver.com>
2026-02-13 22:53:00 +0900

    Exception핸들러

---
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,33 @@
+package hello.exception.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public @Nullable ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, @Nullable Object handler, Exception ex) {
+
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentExcpeiton resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView();
+            }
+
+        } catch (IOException e) {
+            log.error("resolver ex",e);
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
