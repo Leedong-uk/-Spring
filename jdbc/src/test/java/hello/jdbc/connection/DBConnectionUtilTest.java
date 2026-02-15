9f46948edc544ef8f56f1ac0c0f432e5362d15fe
이동욱 <du123kim@naver.com>
2026-02-15 17:23:30 +0900

    JDBC 의 필요성

--- jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -1,4 +1,18 @@
 package hello.jdbc.connection;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
 public class DBConnectionUtilTest {
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        Assertions.assertThat(connection).isNotNull();
+
+
+    }
+
+
 }
