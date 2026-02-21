fdb4136fdc24f35904d9a928592face6a3603947
이동욱 <du123kim@naver.com>
2026-02-22 00:04:14 +0900

    도메인 모델 패턴 , 트랜잭션 스크립트 패턴 연습

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //== 비지니스 로직 == //
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int realStock = this.stockQuantity - quantity;
+        if (realStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = realStock;
+    }
 
 }
