cb1f3fa73078a942824dd461e5f9378a31702bd9
이동욱 <du123kim@naver.com>
2026-02-25 21:57:10 +0900

    XToOne 성능 최적화

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * X to one (ManyToOne, OneToOne)
+ * Order
+ * Order -> Member (Many to One)
+ * Order -> Delivery (One to One)
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        return all;
+    }
+
+}
