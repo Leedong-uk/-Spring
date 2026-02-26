b2ae67974d3ad3451ab82051229587f069dd8218
이동욱 <du123kim@naver.com>
2026-02-26 23:37:29 +0900

    OSIV 끄고 커맨드와 쿼리 분리하기

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -74,24 +74,24 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
-    @GetMapping("/api/v5/orders")
-    public List<OrderQueryDto> ordersV5() {
-        return orderQueryRepository.findAllByDto_optimization();
-    }
-
-    @GetMapping("/api/v6/orders")
-    public List<OrderFlatDto> ordersV6() {
-        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
-        return flats.stream()
-                .collect(groupingBy(
-                        o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress(), e.getValue()),
-                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())))
-                .entrySet().stream()
-                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
-                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
-                        e.getKey().getAddress(), e.getValue())).collect(toList());
-
-    }
+//    @GetMapping("/api/v5/orders")
+//    public List<OrderQueryDto> ordersV5() {
+//        return orderQueryRepository.findAllByDto_optimization();
+//    }
+//
+//    @GetMapping("/api/v6/orders")
+//    public List<OrderFlatDto> ordersV6() {
+//        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+//        return flats.stream()
+//                .collect(groupingBy(
+//                        o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress(), e.getValue()),
+//                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())))
+//                .entrySet().stream()
+//                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+//                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+//                        e.getKey().getAddress(), e.getValue())).collect(toList());
+//
+//    }
 
 
     @Data
