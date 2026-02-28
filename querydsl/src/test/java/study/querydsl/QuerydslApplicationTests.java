02ea3aea94752ec4b9c42da4b0cbecc0f016f7a5
Leedong-uk <du123kim@naver.com>
2026-02-28 19:52:07 +0900

    기본 엔티티

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,34 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
 	}
 
 }
