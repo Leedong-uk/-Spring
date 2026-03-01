ef5da0eb2b7d14a6ad45993fa04356a3f23dbb98
Leedong-uk <du123kim@naver.com>
2026-03-01 22:06:48 +0900

    Querydsl+SpringDataJPA = CustomRepository

---
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,86 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired JPAQueryFactory queryFactory;
+    @Autowired EntityManager em;
+
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    @DisplayName("basicTest")
+    void basicTest() throws Exception {
+        //given
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        List<Member> result1 = memberRepository.findAll();
+        List<Member> result2 = memberRepository.findByUsername("member1");
+
+        //then
+        assertThat(findMember).isEqualTo(member);
+        assertThat(result1).contains(member);
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    @DisplayName("searchTest")
+    void searchTest() throws Exception{
+        //given
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+
+        //when
+        List<MemberTeamDto> result = memberRepository.search(condition);
+
+        //then
+        assertThat(result).extracting("username").containsExactly("member4");
+
+    }
+
+}
