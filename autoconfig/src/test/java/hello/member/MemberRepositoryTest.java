bdc23c4267471422138c7dc361c6d91641a63604
이동욱 <du123kim@naver.com>
2026-03-02 00:06:30 +0900

    dataSource , TransacitonManger 수동 등록

---
+++ autoconfig/src/test/java/hello/member/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package hello.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    @Test
+    @DisplayName("memberTest")
+    void memberTest() throws Exception {
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getMemberId());
+
+        assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+
+
+    }
+
+
+}
